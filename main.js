507dfc43f0dcda95081724d4f4d282a9eb17f7cc
David Nelson <nelsondavidmark@gmail.com>
2019-02-15 14:31:32 +0700

    Add AJAX call to form

--- main.js
+++ main.js
@@ -3,15 +3,64 @@ function addToPokedex(event) {
   let name = event.target.pokemon.value;
   let description = event.target.description.value;
 
-  let listItem = document.createElement('li');
-  let list = document.getElementById('pokemon-list');
+  let pokemonContainer = document.createElement('div');
+  pokemonContainer.id = name.toLowerCase();
+  let pokemonContent = document.createElement('p');
+  pokemonContent.innerHTML = `${name} - ${description}`;
+  pokemonContainer.appendChild(pokemonContent);
 
-  if (name === '' || description === '') {
-  	alert('Please add a pokemon name and description.');
-  } else {
-  	listItem.innerHTML = `${name} - ${description}`;
-  	list.appendChild(listItem);
-  }
+  let list = document.getElementById('pokemon-list');
+  list.appendChild(pokemonContainer);
+
+  let loadingIndicator = createLoadingIndicator();
+  pokemonContainer.appendChild(loadingIndicator);
+}
+
+function fetchPokemon(event) {
+  let pokemon = event.target.pokemon.value.toLowerCase();
+
+  let request = new XMLHttpRequest();
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+  request.open('GET', url);
+  request.send();
+
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        let response = JSON.parse(request.responseText);
+
+        placePokemonImage(response);
+      } else {
+        console.log('There was a problem with the request.');
+      }
+    }
+  };
+}
+
+function placePokemonImage(pokemonData) {
+  removeLoadingIndicator();
+
+  let path = pokemonData.sprites.front_default;
+  let name = pokemonData.name;
+
+  let image = document.createElement('img');
+  image.src = path;
+  image.alt = name;
+
+  let pokemonListItem = document.getElementById(name.toLowerCase());
+  pokemonListItem.appendChild(image); 
+}
+
+function createLoadingIndicator() {
+  let loading = document.createElement('p')
+  loading.id = 'loading';
+  loading.innerHTML = 'Loading...';
+  return loading;
+}
+
+function removeLoadingIndicator() {
+  let loading = document.getElementById('loading');
+  loading.remove();
 }
 
 function clearForm() {
@@ -22,6 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let form = document.getElementById('pokedex');
   form.addEventListener('submit', function() {
     addToPokedex(event);
+    fetchPokemon(event);
     clearForm();
   });
 });
